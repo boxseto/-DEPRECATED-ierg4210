27753f65bb34e7778765b13b8cd3f40e3fd3906a
boxseto <35777054+boxseto@users.noreply.github.com>
2018-10-26 20:02:12 +0800

    tmp4

--- update.php
+++ update.php
@@ -1,41 +1,23 @@
 <?php
-//credit to alex
 //https://stackoverflow.com/questions/11376315/creating-a-thumbnail-from-an-uploaded-image
-function makeThumbnails($updir, $img)
-{
-    $thumbnail_width = 134;
-    $thumbnail_height = 189;
-    $thumb_beforeword = "thumb";
-    $arr_image_details = getimagesize("$updir" . "$img"); // pass id to thumb name
-    $original_width = $arr_image_details[0];
-    $original_height = $arr_image_details[1];
-    if ($original_width > $original_height) {
-        $new_width = $thumbnail_width;
-        $new_height = intval($original_height * $new_width / $original_width);
-    } else {
-        $new_height = $thumbnail_height;
-        $new_width = intval($original_width * $new_height / $original_height);
-    }
-    $dest_x = intval(($thumbnail_width - $new_width) / 2);
-    $dest_y = intval(($thumbnail_height - $new_height) / 2);
-    if ($arr_image_details[2] == IMAGETYPE_GIF) {
-        $imgt = "ImageGIF";
-        $imgcreatefrom = "ImageCreateFromGIF";
-    }
-    if ($arr_image_details[2] == IMAGETYPE_JPEG) {
-        $imgt = "ImageJPEG";
-        $imgcreatefrom = "ImageCreateFromJPEG";
-    }
-    if ($arr_image_details[2] == IMAGETYPE_PNG) {
-        $imgt = "ImagePNG";
-        $imgcreatefrom = "ImageCreateFromPNG";
-    }
-    if ($imgt) {
-        $old_image = $imgcreatefrom("$updir" . "$img");
-        $new_image = imagecreatetruecolor($thumbnail_width, $thumbnail_height);
-        imagecopyresized($new_image, $old_image, $dest_x, $dest_y, 0, 0, $new_width, $new_height, $original_width, $original_height);
-        $imgt($new_image, "$updir" . "$thumb_beforeword" . "$img");
-    }
+function make_thumb($src, $dest, $desired_width) {
+
+    /* read the source image */
+    $source_image = imagecreatefromjpeg($src);
+    $width = imagesx($source_image);
+    $height = imagesy($source_image);
+
+    /* find the "desired height" of this thumbnail, relative to the desired width  */
+    $desired_height = floor($height * ($desired_width / $width));
+
+    /* create a new, "virtual" image */
+    $virtual_image = imagecreatetruecolor($desired_width, $desired_height);
+
+    /* copy source image at a resized size */
+    imagecopyresampled($virtual_image, $source_image, 0, 0, 0, 0, $desired_width, $desired_height, $width, $height);
+
+    /* create the physical thumbnail image to its destination */
+    imagejpeg($virtual_image, $dest);
 }
 $pid = htmlspecialchars($_POST['pid']);
 $conn = new mysqli("localhost", "root", "toor", "IERG4210");
@@ -76,11 +58,7 @@ if($conn->query($sql) === TRUE){
 				$sql = "UPDATE products SET image=\"" . $file_name  . "\" WHERE pid=" . $pid;
 				$conn->query($sql);
 				move_uploaded_file($file_tmp,"img/products/" . $file_name);
-        try {
-          makeThumbnails("img/products/" . $file_name);
-        }catch (Exception $e) {
-          echo $e->getMessage();
-        }
+        make_thumb("img/products/" . $file_name, "img/products/" . "thumb_" . $file_name, "200");
 				header("Location: admin.php");	
 			}else{
 				echo "fail file upload";
